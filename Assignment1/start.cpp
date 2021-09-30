766fe652018ec818f400130f76facafb7f8e72a4
Peekoe <peekoecode@gmail.com>
2021-09-30 16:32:44 -0700

    make function to get user input, need parsing function to validate input

--- Assignment1/start.cpp
+++ Assignment1/start.cpp
@@ -1,6 +1,47 @@
 #include <iostream>
+#include <vector>
+using namespace std;
 
-int main()
+vector<string> getInput(vector<string> output, int& count);
+
+struct Process
 {
+    string id;
+    int at;
+    int bt;
+};
+
+int main(int argc, char *argv[])
+{
+    vector<Process> p;
+    vector<string> params;
+    string input;
+    int inputCount = 0;
+    vector<string> output = { "Please enter the process name", "Please enter AT", "Please enter BT" };
+
+    params = getInput(output, inputCount);
+
+    // write function to parse user inputs
+
     return 0;
+}
+
+vector<string> getInput(vector<string> output, int& count)
+{
+    string input;
+    vector<string> params;
+
+    while (input != "done")
+    {
+        // each process has 3 pieces of data
+        cout << output[count % 3];
+        cin >> input;
+        if (input != "done")
+        {
+            params.push_back(input);
+            count++;
+        }
+    }
+
+    return params;
 }
