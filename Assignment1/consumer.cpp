3ed582ff0c408d1f179021333b57851444d8030d
Jeff Martz <jeffmartz11@gmail.com>
2021-10-04 17:43:18 -0700

    Consumer read memory and output string vector

--- Assignment1/consumer.cpp
+++ Assignment1/consumer.cpp
@@ -7,6 +7,8 @@
 #include <sys/mman.h>
 #include <vector>
 #include <string>
+#include <sstream>
+
 using namespace std;
 
 vector<string> consumer()
@@ -31,11 +33,26 @@ vector<string> consumer()
     printf("%s", (char *)ptr);
 
     // read from memory and put into vector
+    char *charptr = (char *)ptr; // Create char pointer and make it equal to typecast ptr
+    std::vector<std::string> output; // Create vector for the function to output
+    std::string str; // Create holding variable for the string values
+    do {
+        if (*charptr != (',' || NULL)){ // Check if delimiter is hit, or the end has been reached
+            str += *charptr; // Add character to string variable
+            charptr++; // Move forward in memory
+        }
+        else { // Execute if delimiter is hit
+            output.push_back(str); // Add string as an element to the output vector
+            str.clear(); // clear the string so that new info can be held
+            charptr++; // Move forward in memory
+        }
+    } while (charptr != '\0'); // Continue loop unless the end was reached
+
 
     // remove the shared memory object
     shm_unlink(name);
 
     // return string vector, (# of processes, input...)
-
-    return 0;
+    return output;
+    //return 0;
 }
